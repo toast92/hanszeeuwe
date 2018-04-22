9b28f40a8b176b0072d34f75fbd7de9d4cbbd788
Irina Nicula <irina.nicula0@gmail.com>
2018-04-22 13:49:01 +0200

    created image gallery, not fully functional

--- index.php
+++ index.php
@@ -12,14 +12,36 @@
         <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); 
         ?>
       </header>
-      <div class="main">
 
-      
+      <div class="main"> 
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post()?>
+        <!-- contents of the loop -->
+          <div class="schilderijen">
+            <h2 class="page-title"><?php the_title(); ?></h2>
+
+            <h1><?php the_field('hello'); ?></h1>
+
+            <?php $images = get_field('gallery');
+            $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
+
+            if( $images ): 
+            ?>
+                <ul>
+                    <?php foreach( $images as $image ): ?>
+                        <li>
+                          <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+
+          </div>
+        <?php endwhile; endif; ?> <!-- loop is closed -->
       </div>
+
       <footer>
             <?php include 'footer.php'; ?>
       </footer>
     </div>
-
   </body>
 </html>
