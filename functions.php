212da6afab77541ce2f08c7e6c9f9654aafc7ff6
Irina Nicula <irina.nicula0@gmail.com>
2018-05-03 13:26:24 +0200

    Gallery functional, added lightbox, can add 4 or 6 thumbs on row, added contact form

--- functions.php
+++ functions.php
@@ -2,3 +2,5 @@
   register_nav_menu('main-menu',__( 'main menu' ));
 }
 add_action( 'init', 'wpb_custom_new_menu' );?>
+
+<?php add_image_size( 'my-thumbnail', 400, 200, true);?>
--- schetjes.php
+++ schetjes.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="viewport" content="width=device-width" />
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/reset.css' ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/resources/lightbox.min.css' ?>">
@@ -24,26 +23,21 @@
       </header>
     <div class="container">
       
+
       <div class="main"> 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post()?>
-        <h2 class="page-title"><?php the_title(); ?></h2>
-        <?php the_content();?>
-          <div class="schilderijen <?php the_field('amount') ?>">
+          <div class="schetsen">
+            <h2 class="page-title"><?php the_title(); ?></h2>
+
             <?php $images = get_field('gallery');
+
             if($images): 
               foreach( $images as $image ):?>
-              <div class="thumbnail">
                 <a 
-                  href="<?php echo $image['sizes']['large']; ?>"
-                  data-lightbox="paintings"
-                  data-title="<?php echo $image['description']; ?>">
+                  href="<?php echo $image['sizes']['large'] ?>"
+                  data-lightbox="paintings">
                   <?php echo wp_get_attachment_image( $image['ID'], 'thumbnail'); ?>
-  
-                  <!-- -->
                 </a>
-
-            </div>
-                <?php add_image_size( 'my-thumbnail', 400, 200, true);?>
               <?php endforeach; ?>
             <?php endif; ?>
           </div>
