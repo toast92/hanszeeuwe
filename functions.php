c7eab4d4ad1769eca8d3696728fd4abfe11b4370
Irina Nicula <irina.nicula0@gmail.com>
2018-05-06 01:49:13 +0200

    final version

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php function wpb_custom_new_menu() {
   register_nav_menu('main-menu',__( 'main menu' ));
 }
-add_action( 'init', 'wpb_custom_new_menu' );?>
 
-<?php add_image_size( 'my-thumbnail', 400, 200, true);?>
+add_action( 'init', 'wpb_custom_new_menu' );
+
+add_image_size( 'my-thumbnail', 400, 200, true);?>
--- index.php
+++ index.php
@@ -5,6 +5,50 @@
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/reset.css' ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/resources/lightbox.min.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/resources/wpforms-full.css'?>">
+    <title>Hans Zeeuwe</title>
+  </head>
+  <body>
+  <header>
+    <div class="menu">
+        <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) );?>
+    </div>
+  </header>
+
+  <div class="container">    
+    <div class="main"> 
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post()?>
+        <h2 class="page-title"><?php the_title(); ?></h2>
+        <?php the_content();?>
+        <div class="gallery <?php the_field('amount') ?>">
+          <?php $images = get_field('gallery');
+          if($images): 
+            foreach( $images as $image ):?>
+              <div class="thumbnail">
+                <a 
+                  href="<?php echo $image['sizes']['large']; ?>"
+                  data-lightbox="gallery-item"
+                  data-title="<?php echo $image['description']; ?>">
+                  <?php echo wp_get_attachment_image( $image['ID'], 'thumbnail'); ?>
+                </a>
+              </div>
+            <?php endforeach;
+          endif; ?>
+        </div>
+      <?php endwhile; endif; ?> <!-- loop is closed -->
+    </div>
+
+    <footer>
+      <ul>
+        <li>CONTACT</li>
+        <li>06-13170468</li>
+        <li>contact@hanszeeuwe.nl</li>
+        <li>Achtste donk 126</li>
+        <li>5233PE, Den Bosch</li>
+      </ul>
+    </footer>
+    </div>
+
     <script src="<?php echo get_template_directory_uri(). '/resources/lightbox-plus-jquery.js' ?>"></script>
     <script>
     lightbox.option({
@@ -12,47 +56,6 @@
       'wrapAround': true,
       'imageFadeDuration': 400
     })
-</script>
-    <title>Hans Zeeuwe</title>
-  </head>
-  <body>
-  <header>
-    <div class="menu">
-        <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); 
-        ?>
-    </div>
-      </header>
-    <div class="container">
-      
-      <div class="main"> 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post()?>
-        <h2 class="page-title"><?php the_title(); ?></h2>
-        <?php the_content();?>
-          <div class="schilderijen <?php the_field('amount') ?>">
-            <?php $images = get_field('gallery');
-            if($images): 
-              foreach( $images as $image ):?>
-              <div class="thumbnail">
-                <a 
-                  href="<?php echo $image['sizes']['large']; ?>"
-                  data-lightbox="paintings"
-                  data-title="<?php echo $image['description']; ?>">
-                  <?php echo wp_get_attachment_image( $image['ID'], 'thumbnail'); ?>
-  
-                  <!-- -->
-                </a>
-
-            </div>
-                <?php add_image_size( 'my-thumbnail', 400, 200, true);?>
-              <?php endforeach; ?>
-            <?php endif; ?>
-          </div>
-        <?php endwhile; endif; ?> <!-- loop is closed -->
-      </div>
-
-      <footer>
-            <?php include 'footer.php'; ?>
-      </footer>
-    </div>
+    </script>
   </body>
 </html>
